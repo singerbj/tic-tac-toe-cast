1ed657163027bc2a3fed1b4ba3621b3300fcdf92
Benjamin Singer <benjaminjsinger@gmail.com>
2022-02-12 11:44:24 -0600

    progress maybe i dono

--- cast-receiver/src/App.js
+++ cast-receiver/src/App.js
@@ -1,24 +1,226 @@
+import { useState, useEffect, useRef } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Peer from 'peerjs';
+import Config from "./Config";
 
-function App() {
+if(window.navigator.userAgent.indexOf('CrKey') > -1){
+  const options = new window.cast.framework.CastReceiverOptions();
+  options.disableIdleTimeout = true;
+
+  const instance = window.cast.framework.CastReceiverContext.getInstance();
+  instance.start(options);
+}
+
+const makeid = (length) => {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return Config.HOST_ID_PREFIX + '-' + result;
+};
+
+const ticTacToeSquareStyle = { display: "inline-block", width: 100, height: 100, backgroundColor: "#ccc", border: "3px solid #fff"};
+
+const App = () => {
+  const [hostId, setHostId] = useState();
+  const [connections, setConnections] = useState([]);
+  const [players, setPlayers] = useState({});
+  const [gameState, setGameState] = useState(0);
+  const [state, setState] = useState([]);
+
+  const playersRef = useRef(players);
+  const gameStateRef = useRef(gameState);
+  const stateRef = useRef(state);
+
+
+  useEffect(() => {
+    var serverId = makeid(6);
+    var peer = new Peer(serverId, { 
+      host: 'localhost',
+      port: 9000,
+      path: '/myapp', 
+      debug: 3
+    });
+
+    var connectionsArray = [];
+    peer.on('open', function () {
+        if (peer.id === null) {
+            console.log('Received null id from peer open');
+        } else {
+            setHostId(peer.id);
+        }
+    });
+
+    peer.on('connection', function (conn) {
+      const playerName = conn.metadata ? conn.metadata.playerName : undefined;
+      const deviceId = conn.metadata ? conn.metadata.deviceId : undefined;
+      let numberOfPlayers = Object.keys(playersRef.current).length;
+
+      if(playerName && deviceId){
+        let gameStarted = gameState !== Config.GAME_STATE.PLAYERS_CONNECT;
+        let morePlayersCanJoin = numberOfPlayers < Config.MAX_PLAYERS;
+        let playerHasConnectedBefore = playersRef.current[deviceId];
+
+        if((!gameStarted && morePlayersCanJoin) || (gameStarted && playerHasConnectedBefore)){
+          console.log("=============> Connected to: " + conn.peer + " with playerName: " + playerName);
+          connectionsArray.push(conn);
+          
+          setPlayers((prevPlayers) => {
+            return { 
+              ...playersRef.current, 
+              [deviceId]: {
+                deviceId,
+                playerId: conn.peer,
+                playerName,
+                connected: true,
+                lastMessage: Date.now(),
+                isHost: Object.keys(prevPlayers).length === 0,
+              }
+            }
+          });
+
+          conn.on('data', function(data) {
+            console.log('data recieved from ' + conn.peer + ':', data);
+            // handle heartbeat
+            // if(data.type === Config.MESSAGE_TYPES.HEARTBEAT){
+            //   setPlayers((players) => {
+            //     if(players[data.deviceId]){
+            //       return {
+            //         ...players,
+            //         [data.deviceId]: {
+            //           ...players[data.deviceId],
+            //           lastMessage: Date.now()
+            //         }
+            //       };
+            //     } else {
+            //       console.error("Invalid HEARTBEAT recieved");
+            //       return players;
+            //     } 
+            //   });
+            // }
+            if(data.type === Config.MESSAGE_TYPES.DISCONNECT){
+              setPlayers((players) => {
+                if(players[data.deviceId]){
+                  const playersCopy = {...players};
+                  delete playersCopy[data.deviceId];
+                  return playersCopy;
+                } else {
+                  console.error("Invalid DISCONNECT recieved");
+                  return players;
+                } 
+              });
+            }
+            // handle start game
+            // handle a player's move
+          });
+          conn.on('close', function(data) {
+            console.log('close (conn) ' + conn.peer + ':', data);
+          });
+          conn.on('error', function(data) {
+            console.log('error (conn) ' + conn.peer + ':', data);
+          });
+        } else {
+          if(gameStarted){
+            console.log("Game already started, closing connection.");
+            conn.send({ type: "GAME_STARTED" });
+            setTimeout(() => { conn.close(); }, 1000);
+          } else if(!gameStarted) {
+            console.log("Too many connected players, closing connection.");
+            conn.send({ type: "TOO_MANY_PLAYERS" });
+            setTimeout(() => { conn.close(); }, 1000);
+          }
+        }
+      } else {
+        console.log("Invalid Game connection, closing connection.");
+        conn.send({ type: "GAME_STARTED"});
+        setTimeout(() => { conn.close(); }, 1000);
+      }
+    });
+
+    peer.on('disconnected', function (conn) {
+        console.log('Connection lost. Please reconnect', conn);
+    });
+
+    peer.on('close', function() {
+        // conn = null;
+        console.log('Connection destroyed');
+    });
+
+    peer.on('error', function (err) {
+        console.log(err);
+    });
+
+    const intervalId = setInterval(() => {
+      setPlayers((oldPlayers) => {
+        const players = { ...oldPlayers };
+        Object.keys(players).forEach((deviceId) => {
+          let player = players[deviceId];
+          if(player.connected && player.lastMessage < (Date.now() - Config.MAX_HEARTBEAT_INTERVAL)){
+            player.connected = false;
+            if(gameState === Config.GAME_STATE.PLAYERS_CONNECT){
+              if(player.isHost){
+                player.isHost = false;
+                var nextHost = Object.keys(players).filter((deviceId) => deviceId !== player.deviceId)[0];
+                if(nextHost){
+                  nextHost.isHost = true;
+                }
+              }
+            }
+          } else if(!player.connected && player.lastMessage >= (Date.now() - Config.MAX_HEARTBEAT_INTERVAL)){
+            player.connected = true;
+          }
+        });
+        return players;
+      });
+
+      // connectionsArray = connectionsArray.filter((conn) => {
+      //   return conn && conn.send && conn.peerConnection && conn.peerConnection.iceConnectionState !== "disconnected";
+      // });
+      connectionsArray.forEach((conn) => {
+        if(conn && conn.open && conn.send ){
+          conn.send({ type: Config.STATE_LABEL, state });
+        }
+      });
+    }, Config.SEND_STATE_INTERVAL);
+
+    console.log('PeerJs setup complete!');
+    
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        Host Id: {hostId}
+        <br />
+        Game State: {gameState}
+        <br />
+        Players: {JSON.stringify(players)}
+      </div>
+      <br></br>
+      <div className="board">
+        <div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+        </div>
+        <div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+        </div>
+        <div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+          <div style={ticTacToeSquareStyle}>&nbsp;</div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- expo-app/App.js
+++ expo-app/App.js
@@ -1,9 +1,34 @@
-import { useEffect } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { CastButton, useRemoteMediaClient } from 'react-native-google-cast'
+import { useState, useRef } from "react";
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  TextInput,
+  Text,
+  View
+} from "react-native";
+import * as SecureStore from 'expo-secure-store';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+import { mediaDevices, RTCView } from "react-native-webrtc";
+import { CastButton, useRemoteMediaClient } from 'react-native-google-cast';
+import Peer from 'react-native-peerjs';
+import { useEffect } from "react/cjs/react.development";
 
-export default function App() {
+const MAX_SERVER_MESSAGE_INTERVAL = 15000;
+
+const ticTacToeSquareStyle = { backgroundColor: "#ccc", width: 100, height: 100, borderWidth: 3, borderColor: "#fff" };
+
+function App() {
+  const [hostId, setHostId] = useState("");
+  const [deviceId, setDeviceId] = useState();
+  const [connected, setConnected] = useState(false);
+  const [state, setState] = useState({state: [], time: 0});
+
+  const peerRef = useRef(null);
+  const connRef = useRef(null);
+  const connectedRef = useRef(connected);
+  const stateRef = useRef(state);
 
   // This will automatically rerender when client is connected to a device
   // (after pressing the button that's rendered below)
@@ -12,28 +37,175 @@ export default function App() {
   if (client) {
     // Send the media to your Cast device as soon as we connect to a device
     // (though you'll probably want to call this later once user clicks on a video or something)
-    console.log("==================================================>");
     client.loadMedia().catch((e) => {
-      console.error("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",e);
+      console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%", e);
     })
   }
 
-  // This will render native Cast button.
-  // When a user presses it, a Cast dialog will prompt them to select a Cast device to connect to.
-  return (
-    <View style={styles.container}>
-      <Text>Hello wurld wtf</Text>
-      <CastButton style={{ width: 24, height: 24, tintColor: 'black' }} />
-      <StatusBar style="auto" />
-    </View>
-  );
+  const connect = () => {
+    const peer = new Peer(undefined, {
+      secure: false,
+      host: '192.168.181.233',
+      port: 9000,
+      path: '/myapp', 
+      debug: 3
+    });
+    peer.on('error', (error) => {
+      console.error(error);
+    });
+
+    peer.on('open', peerId => {
+      console.log('Remote peer open with ID', peerId);
+
+      const conn = peer.connect("tic-tac-toe-cast-" +  hostId, { 
+        metadata: { 
+          playerName: 'Player ' + Math.floor(Math.random() * 9999) + 1,
+          deviceId
+        } 
+      });
+
+      conn.on('open', () => {
+        console.log('Remote peer has opened connection.');
+        setConnected(true);
+      });
+
+      conn.on('data', (data) => {
+        console.log('Received from local peer', data);
+        if(data.type === "STATE"){
+          setState({ state: data.state, time: Date.now() });
+        }
+      });
+
+      conn.on('close', (data) => {
+        console.log('Connection closed:', data);
+        setConnected(false);
+      });
+
+      conn.on('error', (error) => {
+        console.log('Connection error: ', error);
+        setConnected(false);
+      });
+      
+      console.log("1 &&&&&&&&&&&&&&&&&");
+      connRef.current = conn;
+    });
+
+    peerRef.current = peer;
+  };
+
+  const disconnect = () => {
+    // peerjsDataRef.current.conn.send({ "type": "DISCONNECT", "deviceId": deviceId });
+
+    if(peerRef.current && peerRef.current.disconnect){
+      peerRef.current.disconnect();
+    }
+    if(connRef.current && connRef.current.close){
+      connRef.current.close();
+    }
+
+    console.log("2 &&&&&&&&&&&&&&&&&");
+    connRef.current = null;
+    peerRef.current = null;
+    setConnected(false);
+  };
+
+  const makeMove = () => {
+
+  };
+
+  useEffect(() => {
+    (async () => {
+      let uuid = await SecureStore.getItemAsync('deviceId');
+      if(!uuid){
+        uuid = uuidv4();
+        await SecureStore.setItemAsync('deviceId', JSON.stringify(uuid));
+      }
+      setDeviceId(uuid);
+    })();
+  }, [connected])
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      
+      if(connRef.current){
+        console.log("----->", connRef.current.send);
+      }
+      // if (connRef.current && connRef.current.send && deviceId) {
+      //   console.log(1234);
+      //   connRef.current.send({ "type": "HEARTBEAT", "deviceId": deviceId });
+      // }
+      sendHb();
+      if(connRef.current && (Date.now() - state.time) > MAX_SERVER_MESSAGE_INTERVAL){
+        console.log("Lost connection to server...");
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  });
+
+  const sendHb = () => {
+    if (connRef.current && connRef.current.send && deviceId) {
+      console.log(1234);
+      connRef.current.send({ "type": "HEARTBEAT", "deviceId": deviceId });
+    }
+  };
+
+  if(deviceId === undefined){
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      <Text>Loading...</Text>
+    </SafeAreaView>
+    )
+  } else {
+
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" />
+        { !connected && 
+          <TextInput
+            style={{ margin: 20 }}
+            autoCapitalize = {"characters"}
+            onChangeText={(text) => {
+              setHostId(text.trim());
+            }}
+            value={hostId}
+            placeholder="Enter Host Id"
+          />
+        }
+        { connected && 
+          <Text>Connected to host: {hostId}</Text>
+        }
+        { !connected && 
+          <Button style={{ margin: 20 }} title={'Connect'} onPress={() => { connect() }}></Button>
+        }
+        { connected &&
+          <Button style={{ margin: 20 }} title={'Disconnect'} onPress={() => { disconnect() }}></Button>
+        }
+        <Button style={{ margin: 20 }} title={'Send HB'} onPress={() => { sendHb() }}></Button>
+        <CastButton style={{ margin: 20, width: 24, height: 24, tintColor: 'black' }} />
+        <Text>Connected: {connected.toString()}</Text>
+        <Text>{JSON.stringify(state)}</Text>
+        <View style={{ backgroundColor: "#fff", flexDirection: 'row' }}>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(0)}}></View>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(1)}}></View>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(2)}}></View>
+        </View>
+        <View style={{ backgroundColor: "#fff", flexDirection: 'row' }}>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(3)}}></View>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(4)}}></View>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(5)}}></View>
+        </View>
+        <View style={{ backgroundColor: "#fff", flexDirection: 'row' }}>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(6)}}></View>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(7)}}></View>
+          <View style={ticTacToeSquareStyle} onPress={() => { makeMove(8)}}></View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
